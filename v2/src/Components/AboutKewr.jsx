6802c8641aa1d26af09b75420d95a3e4a9bfff9a
hendrisulistya <id.hendrisulistya@gmail.com>
2023-08-13 14:12:14 +0700

    update description

--- v2/src/Components/AboutKewr.jsx
+++ v2/src/Components/AboutKewr.jsx
@@ -12,20 +12,23 @@ function AboutKewr() {
                     <h2 className="mb-4 text-3xl font-bold lg:text-4xl font-heading text-gray-300">
                         About kewr.org
                     </h2>
-                    <p className="mb-8 leading-loose text-gray-500 dark:text-gray-300">
-                    Kewr is a cutting-edge technology company dedicated to revolutionizing the way we interact with the world
+                    <p className="text-justify mb-8 leading-loose text-gray-500 dark:text-gray-300">
+                    Discover a revolution movement with Kewr.org, funded by Kewr Foundation. We're reshaping technology to empower everyone through innovation, financial liberation, and equal access. Our Tech Labs, Foundation, engaged Decentralized Autonomous Organization system and our Limited Liability Partnership drive change for a fairer, inclusive future. Join us in transforming how tech shapes our world.
                     </p>
                     <h2 className="mb-4 text-3xl font-bold lg:text-4xl font-heading text-gray-300">
-                    A note on names
+                    What is KEWR
                     </h2>
-                    <p className="mb-8 leading-loose text-gray-500 dark:text-gray-300">
-                    It's common for people to confuse names within the kewr landscape, which can lead to poor mental models about how kewr works. Here's a quick explainer to clear things up:
+                    <p className="text-justify mb-8 leading-loose text-gray-500 dark:text-gray-300">
+                    Kewr stands as a trailblazing force in the world of technology, a vision realized through the collaborative might of our Decentralized Autonomous Organization (DAO). From day one, our organization is an experiment in DAO-operated ingenuity, ushering in a new era of decentralized decision-making.
+                    As we embark on a transformative journey, the Foundation, Tech Labs, Capital Investment Arm, and Limited Liability Partnership (LLP) come together, each contributing to our initial operations. Over two years, we're propelling innovation, paving the way for a decentralized future.
+                    Our evolution is guided by the ethos of community empowerment. As time unfolds, we're opening a shared space for the community to participate and own, fostering wider involvement. Our DAO, initially nurtured by our organization, blooms into full community-operated autonomy, amplifying collective voices in shaping a future where technology and empowerment unite.
                     </p>
                     <h2 className="mb-4 text-3xl font-bold lg:text-4xl font-heading text-gray-300">
-                    Kewr
+                    Mission
                     </h2>
-                    <p className="mb-8 leading-loose text-gray-500 dark:text-gray-300">
-                    Kewr Org is a knowledge base that upholds the values of inclusion and equalisation of opportunity. We are a platform for critical thinkers and future leaders who are passionate about developing their potential. In the spirit of liberating knowledge, Kewr Org is determined to break the shackles of information barriers and embrace diverse viewpoints.
+                    <p className="text-justify mb-8 leading-loose text-gray-500 dark:text-gray-300">
+                    We are a pioneering force driving humanity into a decentralized technological era. Guided by principles of openness, equity, and innovation, our mission is clear: to empower individuals and communities worldwide with boundary-breaking technology. Our vision? A world where technology is a beacon of freedom, granting unrestricted access to information, unbridled expression, and financial autonomy.
+                    Through a multidimensional approach, we're reshaping society's tech interaction, forging a path towards a more inclusive, equitable future.
                     </p>
                 </div>
             </div>
